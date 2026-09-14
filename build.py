eda47c969aa4472d3d5ba9a5f16109124ba05d90
maithienson999 <88213019+maithienson999@users.noreply.github.com>
2026-09-14 10:35:02 +0700

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 import urllib.request
 
 # Link AdGuard DNS filter chuẩn
-URL = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_16.txt"
+URL = "https://github.com/abpvn/abpvn/blob/master/filter/abpvn_adguard.txt"
 
 try:
     req = urllib.request.Request(URL, headers={'User-Agent': 'Mozilla/5.0'})
